be9bb1ae1593e0f30f91934b60566d233aa9a297
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-06 14:06:42 -0500

    created document structure for collections

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,15 +1,60 @@
 import { Collection, ObjectId } from "mongodb";
 
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE",
+}
+
+export interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+
+export interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
+}
+
 export interface Listing {
   _id: ObjectId;
+  title: string;
+  description: string;
+  image: string;
+  //below is one to one relationship
+  host: string;
+  type: ListingType;
+  address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
+  price: number;
+  numOfGuests: number;
 }
 
 export interface User {
-  _id: ObjectId;
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  //below is type string or undefined
+  walletId?: string;
+  income: number;
+  //below is one to many relationship
+  bookings: ObjectId[];
+  listings: ObjectId[];
 }
 
 export interface Booking {
   _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
 
 export interface Database {
