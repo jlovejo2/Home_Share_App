626612a39287abe3c9347637eac1ccc8bda314c2
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-06 16:51:52 -0500

    set-up graphql resolvers for query OAuth2 and the having mutations for logging in and out

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -3,12 +3,24 @@ import { gql } from "apollo-server-express";
 //using es6 feature here called tagged template literals
 //gql is a tag or function where template literal arguement is applied next to it
 export const typeDefs = gql`
+  type Viewer {
+    id: ID
+    token: String
+    avatar: String
+    hasWallet: Boolean
+    didRequest: Boolean!
+  }
+
   type Query {
     authUrl: String!
   }
 
+  input LogInInput {
+    code: String!
+  }
+
   type Mutation {
-    logIn: String!
-    logOut: String!
+    logIn(input: LogInInput): Viewer!
+    logOut: Viewer!
   }
 `;
