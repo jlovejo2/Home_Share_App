e1c8ee60e3179b039e2d9af3cbaf2c1326a925de
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-10 21:15:04 -0500

    added code to server to create cookie on logIn and clear it on logOut, supposed to be set to browser, currently not happening

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -1,14 +1,24 @@
 import crypto from "crypto";
+import { Request, Response } from "express";
 import { IResolvers } from "apollo-server-express";
-import { Viewer, Database, User } from "../../../lib/types";
 import { Google } from "../../../lib/api";
+import { Viewer, Database, User } from "../../../lib/types";
 import { LogInArgs } from "./types";
+
+const cookieOptions = {
+  httpOnly: true,
+  sameSite: true,
+  signed: true,
+  secure: process.env.NODE_ENV === "development" ? false : true,
+};
 
 const logInViaGoogle = async (
   code: string,
   token: string,
-  db: Database
+  db: Database,
+  res: Response
 ): Promise<User | undefined> => {
+  console.log("logging in with google");
   const { user } = await Google.logIn(code);
 
   if (!user) {
@@ -76,6 +86,37 @@ const logInViaGoogle = async (
     viewer = insertResult.ops[0];
   }
 
+  console.log("about to create cookie ");
+  // console.log(res);
+  res.cookie("viewer", userId, {
+    ...cookieOptions,
+    maxAge: 365 * 24 * 60 * 60 * 1000,
+  });
+
+  console.log("userId", userId);
+  console.log("created cookie");
+  return viewer;
+};
+
+const logInViaCookie = async (
+  token: string,
+  db: Database,
+  req: Request,
+  res: Response
+): Promise<User | undefined> => {
+  const updateRes = await db.users.findOneAndUpdate(
+    { _id: req.signedCookies.viewer },
+    { $set: { token } },
+    { returnOriginal: false }
+  );
+
+  //eslint-disable-next-line
+  let viewer = updateRes.value;
+
+  if (!viewer) {
+    res.clearCookie("viewer", cookieOptions);
+  }
+
   return viewer;
 };
 
@@ -93,15 +134,15 @@ export const viewerResolvers: IResolvers = {
     logIn: async (
       _root: undefined,
       { input }: LogInArgs,
-      { db }: { db: Database }
+      { db, req, res }: { db: Database; req: Request; res: Response }
     ): Promise<Viewer> => {
       try {
         const code = input ? input.code : null;
         const token = crypto.randomBytes(16).toString("hex");
 
         const viewer: User | undefined = code
-          ? await logInViaGoogle(code, token, db)
-          : undefined;
+          ? await logInViaGoogle(code, token, db, res)
+          : await logInViaCookie(token, db, req, res);
 
         if (!viewer) {
           return { didRequest: true };
@@ -118,8 +159,14 @@ export const viewerResolvers: IResolvers = {
         throw new Error(`Failed to log in: ${error}`);
       }
     },
-    logOut: (): Viewer => {
+    logOut: (
+      _root: undefined,
+      _args: {},
+      { res }: { res: Response }
+    ): Viewer => {
       try {
+        //pass options here because most browsers will only clear the cookie if given options are identical to those in given cookie function
+        res.clearCookie("viewer", cookieOptions);
         return { didRequest: true };
       } catch (error) {
         throw new Error(`Failed to log out ${error}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ require("dotenv").config();
 // const express = require("express");
 //typeScript supports import
 import express, { Application } from "express";
+import cookieParser from "cookie-parser";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./graphql";
 import { connectDatabase } from "./database";
@@ -14,6 +15,8 @@ import { connectDatabase } from "./database";
 //mount function excepts the express app instance
 const mount = async (app: Application) => {
   const db = await connectDatabase();
+
+  app.use(cookieParser(process.env.SECRET));
   //creating instance of ApolloServer
   //passing in options needed to instantiate the apolloServer instance
   //ApolloSever expects an existing schema or object with typeDefs and resolvers to be passed into it
@@ -21,7 +24,7 @@ const mount = async (app: Application) => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({ req, res }) => ({ db, req, res }),
   });
   //specify middleware to work alongisde existing server middleware
   //passing in express app instance and where graphql api should live
@@ -32,8 +35,8 @@ const mount = async (app: Application) => {
   console.log(`[app]: http://localhost:${process.env.PORT}`);
 
   //The below two lines of code are used to check that the connection to the database is made and the data is then console logged
-  const listings = await db.listings.find({}).toArray();
-  console.log(listings);
+  // const listings = await db.listings.find({}).toArray();
+  // console.log(listings);
 };
 
 //dismount function runs the express()
