3ae3c618f7f9750e7b2dd52c43af1018b7e7e937
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-13 15:19:37 -0500

    added Client-side loading notification on logIn

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import { Layout, Affix } from "antd";
+import ApolloClient, { from } from "apollo-boost";
+import { ApolloProvider, useMutation } from "react-apollo";
+import { Layout, Affix, Spin } from "antd";
 import {
   AppHeader,
   Home,
@@ -14,6 +14,12 @@ import {
   NotFound,
   User,
 } from "./sections";
+import { AppHeaderSkeleton, ErrorBanner } from "./lib/components";
+import { LOG_IN } from "./lib/graphql/mutations";
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from "./lib/graphql/mutations/LogIn/__generated__/LogIn";
 import { Viewer } from "./lib/types";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
@@ -33,10 +39,39 @@ const initialViewer: Viewer = {
 };
 const App = () => {
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  const [logIn, { error }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: (data) => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+      }
+    },
+  });
 
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    logInRef.current();
+  }, []);
+
+  //if the mutation doesnt show errors or the login request is not completed in some way display a spinner
+  if (!viewer.didRequest && !error) {
+    return (
+      <Layout className="app-skeleton">
+        <AppHeaderSkeleton />
+        <div className="app-skeleton_spinner">
+          <Spin size="large" tip="Launching Tinyhouse" />
+        </div>
+      </Layout>
+    );
+  }
+
+  const LogInErrorBannerElement = error ? (
+    <ErrorBanner description="We weren't able to verify your log in.  Please try again later." />
+  ) : null;
   return (
     <Router>
       <Layout id="app">
+        {LogInErrorBannerElement}
         {/*Affix is a component from ant design that makes what is wrapped to it be a fixed position */}
         <Affix offsetTop={0}>
           <AppHeader viewer={viewer} setViewer={setViewer} />
