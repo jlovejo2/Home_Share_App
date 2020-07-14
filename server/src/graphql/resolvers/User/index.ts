fbee0cf25a16691a1cf3033eb753c5a33f50aea1
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-13 21:03:15 -0500

    added UserArgs interace and initial find user resolver function

--- server/src/graphql/resolvers/User/index.ts
+++ server/src/graphql/resolvers/User/index.ts
@@ -1,9 +1,25 @@
 import { IResolvers } from "apollo-server-express";
+import { Database, User } from "../../../lib/types";
+import { UserArgs } from "./types";
 
 export const userResolvers: IResolvers = {
   Query: {
-    user: () => {
-      return "Query.user";
+    user: async (
+      _root: undefined,
+      { id }: UserArgs,
+      { db }: { db: Database }
+    ): Promise<User> => {
+      try {
+        const user = await db.users.findOne({ _id: id });
+
+        if (!user) {
+          throw new Error("user can't be found");
+        }
+
+        return user;
+      } catch (error) {
+        throw new Error(`Failed to query user: ${error}`);
+      }
     },
   },
 };
