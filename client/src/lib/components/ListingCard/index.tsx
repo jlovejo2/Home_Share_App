a6e7590ddd0ed32ca80532e54cb9310a0beadabc
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-15 11:38:51 -0500

    updated user query on client side to include bookings and listings information, created listingCard component

---
+++ client/src/lib/components/ListingCard/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Card, Icon, Typography } from "antd";
+
+interface Props {
+  listing: {
+    id: string;
+    title: string;
+    image: string;
+    address: string;
+    price: number;
+    numOfGuests: number;
+  };
+}
+
+const { Text, Title } = Typography;
+
+export const ListingCard = ({ listing }: Props) => {
+  const { title, image, address, price, numOfGuests } = listing;
+
+  return (
+    <Card
+      hoverable
+      cover={<div style={{ backgroundImage: `url(${image})` }} />}
+      className="listing-card__cover-img"
+    >
+      <div className="listing-card__details">
+        <div className="listing-card__description">
+          <Title level={4} className="listing-card__price">
+            {price}
+            <span>/day</span>
+          </Title>
+          <Text strong ellipsis className="listing-card__title">
+            {title}
+          </Text>
+          <Text strong ellipsis className="listing-card__address">
+            {address}
+          </Text>
+        </div>
+        <div className="listing-card__dimensions listing-card__dimensions--guest">
+          <Icon type="user" />
+          <Text>{numOfGuests} guests</Text>
+        </div>
+      </div>
+    </Card>
+  );
+};
