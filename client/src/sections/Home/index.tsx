fb1e106e2b8d4da3cd7ecda7d12f9899dbf34710
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-16 13:46:08 -0500

    added home page description section with link to popular US listings to home page

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { RouteComponentProps } from "react-router-dom";
-import { Layout } from "antd";
+import { RouteComponentProps, Link } from "react-router-dom";
+import { Layout, Typography } from "antd";
 import { displayErrorMessage } from "../../lib/utils";
 import { HomeHero } from "./components";
 
 import mapBackground from "./assets/map-background.jpg";
 
 const { Content } = Layout;
+const { Paragraph, Title } = Typography;
 
 export const Home = ({ history }: RouteComponentProps) => {
   const onSearch = (value: String) => {
@@ -25,6 +26,19 @@ export const Home = ({ history }: RouteComponentProps) => {
       style={{ backgroundImage: `url(${mapBackground})` }}
     >
       <HomeHero onSearch={onSearch} />
+      <div className="home__cta-section">
+        <Title>Your guide for all things rental</Title>
+        <Paragraph>
+          Helping you make your best decisions in renting your last minute
+          locations.
+        </Paragraph>
+        <Link
+          to="/listings/united%20states"
+          className="ant-btn ant-btn-primary ant-btn-lg home__cta-section-button"
+        >
+          Popular listings in the United States
+        </Link>
+      </div>
     </Content>
   );
 };
