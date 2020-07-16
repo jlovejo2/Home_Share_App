27316029417fc76224794bd3ab5c644c387f15e1
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-16 13:29:11 -0500

    added map background to home page

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Layout } from "antd";
 import { HomeHero } from "./components";
 
+import mapBackground from "./assets/map-background.jpg";
+
 const { Content } = Layout;
 
 export const Home = () => {
   return (
-    <Content className="home">
+    <Content
+      className="home"
+      style={{ backgroundImage: `url(${mapBackground})` }}
+    >
       <HomeHero />
     </Content>
   );
