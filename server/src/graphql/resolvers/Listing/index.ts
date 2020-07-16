2f94c5b01a10c6ccc1b5c66795d7d69b7bcf2294
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-16 12:58:46 -0500

    created listings resolver to query for all listings and filter them based on price from low to high or high to low

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,7 +1,14 @@
 import { IResolvers } from "apollo-server-express";
 import { Request } from "express";
 import { Database, Listing, User } from "../../../lib/types";
-import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from "./types";
+import {
+  ListingArgs,
+  ListingBookingsArgs,
+  ListingBookingsData,
+  ListingsArgs,
+  ListingsData,
+  ListingsFilter,
+} from "./types";
 import { ObjectId } from "mongodb";
 import { authorize } from "../../../lib/utils";
 
@@ -31,8 +38,38 @@ export const listingsResolver: IResolvers = {
         throw new Error(`Failed to query listing: ${error}`);
       }
     },
-    listings: () => {
-      return "Query.listings";
+    listings: async (
+      _root: undefined,
+      { filter, limit, page }: ListingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingsData> => {
+      try {
+        const data: ListingsData = {
+          total: 0,
+          result: [],
+        };
+
+        let cursor = await db.listings.find({});
+
+        if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+          cursor = cursor.sort({ price: 1 });
+        }
+
+        if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+          cursor = cursor.sort({ price: -1 });
+        }
+        //allows us to skip a specified number of documents
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        //page = 1 limit = 10; cursor starts at 0
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query listings: ${error}`);
+      }
     },
   },
   Listing: {
