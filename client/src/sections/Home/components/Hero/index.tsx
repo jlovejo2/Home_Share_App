9713b45afce168e1ce74cb1fdee013543ebd334d
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-16 13:26:55 -0500

    added homeHero component and created layout and linking for it, added that component to home page

---
+++ client/src/sections/Home/components/Hero/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Card, Col, Input, Row, Typography } from "antd";
+import { Link } from "react-router-dom";
+import torontoImage from "../../assets/toronto.jpg";
+import dubaiImage from "../../assets/dubai.jpg";
+import losAngelesImage from "../../assets/los-angeles.jpg";
+import londonImage from "../../assets/london.jpg";
+
+const { Title } = Typography;
+const { Search } = Input;
+
+export const HomeHero = () => {
+  return (
+    <div className="home-hero">
+      <div className="home-hero__search">
+        <Title>Find a place that fits for you</Title>
+        <Search
+          placeholder="Search 'San Francisco'"
+          size="large"
+          enterButton
+          className="home-hero__search-input"
+        />
+      </div>
+      <Row gutter={12} className="home-hero__cards">
+        <Col xs={12} md={6}>
+          <Link to={`/listings/toronto`}>
+            <Card cover={<img src={torontoImage} alt="Toronto" />}>
+              Toronto
+            </Card>
+          </Link>
+        </Col>
+        <Col xs={12} md={6}>
+          <Link to={`/listings/dubai`}>
+            <Card cover={<img src={dubaiImage} alt="Dubai" />}>Dubai</Card>
+          </Link>
+        </Col>
+        <Col xs={0} md={6}>
+          <Link to={`/listings/los%20angeles`}>
+            <Card cover={<img src={losAngelesImage} alt="Los Angeles" />}>
+              Los Angeles
+            </Card>
+          </Link>
+        </Col>
+        <Col xs={0} md={6}>
+          <Link to={`/listings/london`}>
+            <Card cover={<img src={londonImage} alt="London" />}>London</Card>
+          </Link>
+        </Col>
+      </Row>
+    </div>
+  );
+};
