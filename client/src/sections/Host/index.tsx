6ebe60758ec0c2e3ae287ffbbe00e8ea43ba1b7a
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-21 12:31:10 -0500

    set-up basic layout of host page

--- client/src/sections/Host/index.tsx
+++ client/src/sections/Host/index.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Layout, Typography } from "antd";
+
+const { Content } = Layout;
+const { Text, Title } = Typography;
 
 export const Host = () => {
-  return <h2>Host</h2>;
+  return (
+    <Content className="host-content">
+      <div className="host__form-header">
+        <Title level={3} className="host__form-title">
+          Hi! Let's get start listing your place.
+          <Text type="secondary">
+            In this form, we'll collect some basic and additional information
+            about your listing.
+          </Text>
+        </Title>
+      </div>
+    </Content>
+  );
 };
