5dbc5d15567dd17d1bbb6dc0fc494684cf15b906
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-22 13:25:08 -0500

    added charge function to stripe.ts

--- server/src/lib/api/Stripe.ts
+++ server/src/lib/api/Stripe.ts
@@ -4,7 +4,7 @@ const client = new stripe(`${process.env.S_SECRET_KEY}`, {
   /*eslint-disable @typescript-eslint/ban-ts-ignore */
   //@ts-ignore
   apiVersion: "2019-12-03",
-  /*eslint-disable @typescript-eslint/ban-ts-ignore */
+  /*eslint-enable @typescript-eslint/ban-ts-ignore */
 });
 
 export const Stripe = {
@@ -13,7 +13,7 @@ export const Stripe = {
       /*eslint-disable @typescript-eslint/camelcase */
       grant_type: "authorization_code",
       code,
-      /* eslint-disable @typescript-eslint/camelcase */
+      /* eslint-enable @typescript-eslint/camelcase */
     });
 
     if (!response) {
@@ -21,5 +21,24 @@ export const Stripe = {
     }
 
     return response;
+  },
+  charge: async (amount: number, source: string, stripeAccount: string) => {
+    /*eslint-disable @typescript-eslint/camelcase */
+    const res = await client.charges.create(
+      {
+        amount,
+        currency: "usd",
+        source,
+        application_fee_amount: Math.round(amount * 0.05),
+      },
+      {
+        stripe_account: stripeAccount,
+      }
+    );
+    /*eslint-enable @typescript-eslint/camelcase */
+
+    if (res.status !== "succeeded") {
+      throw new Error("Failed to create charge with Stripe.");
+    }
   },
 };
