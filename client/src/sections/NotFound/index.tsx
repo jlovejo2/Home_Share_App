ab647a9f1bb97e9182edd825cc55e502e7600765
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-27 09:34:18 -0500

    set-up general layout for the 404 page.  with empty and fragment components

--- client/src/sections/NotFound/index.tsx
+++ client/src/sections/NotFound/index.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Empty, Layout, Typography } from "antd";
+
+const { Content } = Layout;
+const { Text } = Typography;
 
 export const NotFound = () => {
-  return <h2>Not Found</h2>;
+  return (
+    <Content className="not-found">
+      <Empty
+        description={
+          <Fragment>
+            <Text className="not-found__desription-title">
+              Uh oh! Something went wrong :(
+            </Text>
+            <hr />
+            <Text className="not-found__description-subtitle">
+              The page you're looking for can't be found
+            </Text>
+          </Fragment>
+        }
+      />
+    </Content>
+  );
 };
