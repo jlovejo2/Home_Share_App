b2c3e552f13c755a1224a21d238f2e5e80388a57
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-12 21:56:51 -0500

    added try catch to cloudinary file

--- server/src/lib/api/Cloudinary.ts
+++ server/src/lib/api/Cloudinary.ts
@@ -2,22 +2,27 @@ import cloudinary from "cloudinary";
 
 export const Cloudinary = {
   upload: async (image: string, folderPath: string) => {
-    let pathPresent = true;
+    try {
+      let pathPresent = true;
 
-    console.log("image: ", image);
-    console.log("folder path: ", folderPath);
-    if (folderPath === "") {
-      pathPresent = false;
+      console.log("image: ", image);
+      console.log("folder path: ", folderPath);
+      if (folderPath === "") {
+        pathPresent = false;
+      }
+      /*eslint-disable @typescript-eslint/camelcase */
+
+      const res = await cloudinary.v2.uploader.upload(image, {
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+        cloud_name: process.env.CLOUDINARY_NAME,
+        folder: pathPresent ? folderPath : "TinyHouse_Assets/",
+      });
+
+      return res.secure_url;
+    } catch (error) {
+      console.log(`Failed to upload to cloudinary: ${error}`);
     }
-    /*eslint-disable @typescript-eslint/camelcase */
-    const res = await cloudinary.v2.uploader.upload(image, {
-      api_key: process.env.CLOUDINARY_KEY,
-      api_secret: process.env.CLOUDINARY_SECRET,
-      cloud_name: process.env.CLOUDINARY_NAME,
-      folder: pathPresent ? folderPath : "TinyHouse_Assets/",
-    });
-
-    return res.secure_url;
   },
   delete: async (image: string) => {
     const res = await cloudinary.v2.uploader.destroy(image, function (result) {
