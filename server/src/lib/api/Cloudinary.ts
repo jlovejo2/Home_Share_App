39f5af71152617e4192317770c5a3b80af334a29
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-12 21:50:19 -0500

    added code to save all listing images in a folder specific to user

--- server/src/lib/api/Cloudinary.ts
+++ server/src/lib/api/Cloudinary.ts
@@ -1,13 +1,20 @@
 import cloudinary from "cloudinary";
 
 export const Cloudinary = {
-  upload: async (image: string) => {
+  upload: async (image: string, folderPath: string) => {
+    let pathPresent = true;
+
+    console.log("image: ", image);
+    console.log("folder path: ", folderPath);
+    if (folderPath === "") {
+      pathPresent = false;
+    }
     /*eslint-disable @typescript-eslint/camelcase */
     const res = await cloudinary.v2.uploader.upload(image, {
       api_key: process.env.CLOUDINARY_KEY,
       api_secret: process.env.CLOUDINARY_SECRET,
       cloud_name: process.env.CLOUDINARY_NAME,
-      folder: "TinyHouse_Assets/",
+      folder: pathPresent ? folderPath : "TinyHouse_Assets/",
     });
 
     return res.secure_url;
