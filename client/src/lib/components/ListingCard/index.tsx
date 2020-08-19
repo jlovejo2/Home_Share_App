df102b00c014abdaf97414213bffa358cbe52a36
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-19 16:24:41 -0500

    added compression middleware, and code for server to direct routes to client folder

--- client/src/lib/components/ListingCard/index.tsx
+++ client/src/lib/components/ListingCard/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card, Typography } from "antd";
-// import { Icon } from "@ant-design/compatible";
 import { UserOutlined } from "@ant-design/icons";
 import { iconColor, formatListingPrice } from "../../utils";
 
